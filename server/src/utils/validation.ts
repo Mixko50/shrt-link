420689f374d3779d16c5ad533b4cc03964a5d4ed
Apisit Maneerat <apisit.mixko@mail.kmutt.ac.th>
2023-03-14 16:15:14 +0700

    fix: regex

--- server/src/utils/validation.ts
+++ server/src/utils/validation.ts
@@ -1,4 +1,4 @@
 export const isAlphanumeric = (text: string): boolean => {
-    const regex = new RegExp(/^[a-z0-9]+$/i)
+    const regex = new RegExp(/^[a-z0-9-_]+$/i)
     return regex.test(text)
 }
