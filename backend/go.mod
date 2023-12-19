845a67269c6cfe018e83d216b6fb71ae1a821958
Mixko <apisitmaneerat@gmail.com>
2023-12-20 03:56:02 +0700

    feat: shrt repo test

--- backend/go.mod
+++ backend/go.mod
@@ -14,6 +14,7 @@ require (
 )
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.1 // indirect
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
--- backend/types/entity/shrt.go
+++ backend/types/entity/shrt.go
@@ -3,10 +3,10 @@ package entity
 import "time"
 
 type Shrt struct {
-	ID        uint      `gorm:"primarykey"`
-	LongURL   string    `gorm:"type:varchar(255)"`
-	Slug      string    `gorm:"type:varchar(255)"`
-	Visit     int       `gorm:"visit"`
+	ID        uint      `gorm:"primarykey,autoIncrement"`
+	LongURL   string    `gorm:"type:varchar(255),not null"`
+	Slug      string    `gorm:"type:varchar(255),not null;unique"`
+	Visit     int       `gorm:"visit,default:0,not null"`
 	CreatedAt time.Time `gorm:"column:createdAt"`
 	UpdatedAt time.Time `gorm:"column:updatedAt"`
 }
